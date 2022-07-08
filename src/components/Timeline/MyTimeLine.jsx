e3d7af6a85080173b7c63d77eb1642033ab86a6b
YazeedSal <yazeedsalameh02082005@gmail.com>
2022-07-08 14:07:25 +0300

    adfg

--- src/components/Timeline/MyTimeLine.jsx
+++ src/components/Timeline/MyTimeLine.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import MyTimelineItem from "./MyTimelineItem";
 
 const MyTimeLine = (props) => {
-  const { expenses, title,handleGetExpenses } = props;
+  const { expenses, title, handleGetExpenses } = props;
   return (
-    <Timeline align="alternate">
+    <div align="alternate">
       <h1>{title}</h1>
       {expenses.map((expense, i) => (
-        <MyTimelineItem key={i} expense={expense} title={expense.title} handleGetExpenses={handleGetExpenses} />
+        <MyTimelineItem
+          key={i}
+          expense={expense}
+          title={expense.title}
+          handleGetExpenses={handleGetExpenses}
+        />
       ))}
-    </Timeline>
+    </div>
   );
 };
 
